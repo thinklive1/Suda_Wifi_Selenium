ba1a6941355dde01b2a713d24015f71a9a6f55d7
thinklive1 <469631989@qq.com>
2025-09-15 12:07:04 +0800

    09/15/2025 12:07:04

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Suda_Wifi_Selenium
+
+## 这是什么?  
+
+一个自动登录suda wifi的python脚本
+
+## 不是有很多自动登录脚本了吗?
+
+根据我的搜索,github上大部分自动登录脚本都是基于网络请求库,也就是完全基于命令行对suda服务器发送登陆请求,这样的好处是,依赖少,纯命令行环境也能用,而缺点则是一旦苏大更改认证请求标准,就得重新改,而且写和改也较为复杂
+本脚本则使用了Selenium(浏览器自动工具,也就是通过脚本像人一样操作浏览器的一个工具),可以模拟人类使用suda wifi登录页,这样的缺点上依赖一个浏览器驱动,配置会麻烦一点,优点则是通用性强,suda一般不会改登录页的前端元素,就是改了重新定位也不难
+
+## 如何使用?
+
+### 对不了解任何编程知识的人
+
+简单地说可以分为几个步骤:
+
+1. 配置python环境,这点可以参考[菜鸟教程](https://www.runoob.com/python3/python3-install.html)
+2. 安装[Selenium](https://www.selenium.dev/zh-cn/documentation/webdriver/getting_started/install_library/),其实就是一句`pip install selenium`
+3. 下载你的浏览器驱动,driver,例如[chormedriver](https://googlechromelabs.github.io/chrome-for-testing/)
+4. 运行项目中的`auto_login.py`,例如在windows终端输入`python auto_login.py`
+
+### 对了解编程知识的人
+
+这里简单介绍一下原理,就是通过类名等属性定位前端元素,然后像人类一样点击选择这些按钮,看代码或者直接喂给ai都能很容易地自定义
+
+
+
---
+++ auto_login.py
@@ -0,0 +1,94 @@
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# 运营商映射关系
+CARRIER_MAP = {
+    "@xyw": "校园网",
+    "@zgyd": "中国移动",
+    "@cucc": "中国联通",
+    "@ctc": "中国电信"
+}
+username = "学号"
+password= "密码"
+
+def login():
+    try:
+        select_element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.NAME, "ISP_select"))
+        )
+        
+        # 创建 Select 对象
+        select = Select(select_element)
+        
+        # 选择指定的选项
+        select.select_by_value("@cucc")  # 通过值选择
+        
+        print(f"已选择选项")
+
+        username_input = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='用户名']"))
+        )
+        username_input.clear()  # 清空输入框
+        username_input.send_keys(username)
+        print(f"已输入用户名: {username}")
+
+        pasword_input = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='密码']"))
+        )
+        
+        # 输入用户名
+        pasword_input.clear()  # 清空输入框
+        pasword_input.send_keys(password)
+
+        login_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//input[@value='登录']"))
+        )
+        login_button.click()
+
+    except Exception as e:
+        print(f"登录出现错误：{e}")
+
+
+def logout(): 
+    try:
+        # 等待注销按钮可见，并点击
+        logout_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.NAME, "logout"))
+        )
+        logout_button.click()
+        print("注销按钮已点击。")
+        confirm_button = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, "boxy-btn1"))
+            )
+        confirm_button.click()
+        back_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.NAME, "GobackButton"))
+        )
+        back_button.click()
+        print("返回登录页")
+    except Exception as e:
+        print(f"注销出现错误：{e}")
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    driver.get("http://10.9.1.3")
+
+    try:
+        login_button = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.XPATH, "//input[@value='登录']"))
+        )
+        if login_button:
+            login()
+        else:
+            print("已经登陆,不执行操作")
+            driver.quit()
+    except Exception:
+        print("登录按钮未找到，未执行任何操作。")
+    time.sleep(5)
+
+
